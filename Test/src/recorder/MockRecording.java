0ceb899d91ca34bfc75ed877d9b8099c4be7f4a2
David Heath <heath.davidanthony@gmail.com>
2014-09-03 15:04:13 -0400

    Fixed errors in Recording Tests

--- Test/src/recorder/MockRecording.java
+++ Test/src/recorder/MockRecording.java
@@ -1,4 +1,10 @@
 package recorder;
 
+import javax.sound.sampled.TargetDataLine;
+
 public class MockRecording implements Recording {
+  @Override
+  public void record(TargetDataLine targetDataLine) {
+
+  }
 }
--- Test/src/recorder/RecordingTests.java
+++ Test/src/recorder/RecordingTests.java
@@ -3,15 +3,13 @@ package recorder;
 import org.junit.Assert;
 import org.junit.Test;
 
-import javax.sound.sampled.AudioFileFormat;
-
 public class RecordingTests {
 
   @Test
   public void testRecordSound() {
     Recording rec = new MockRecording();
     SoundDevice device = new MockSoundDevice(rec);
-    Assert.assertEquals(rec, device.getSound(AudioFileFormat.Type.WAVE));
+    Assert.assertEquals(rec, device.getSound());
   }
 
 
