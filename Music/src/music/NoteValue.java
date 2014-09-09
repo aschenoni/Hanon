738fe1adc0db3871750ad2a659ef9f2a36e09536
David <heath.davidanthony@gmail.com>
2014-09-09 16:46:26 -0400

    Can draw time signature.

--- Music/src/music/NoteValue.java
+++ Music/src/music/NoteValue.java
@@ -27,36 +27,6 @@ public class NoteValue {
   private static final int NUM_HALF_STEPS_IN_OCTAVE = 12;
   private static final float FREQ_CONST = (float) pow(2.0, (1.0 / NUM_HALF_STEPS_IN_OCTAVE));
 
-  //private final NoteName name;
-  //private final int octave;
-
-  /*
-  public static NoteValue fromFrequency(float frequency) {
-    int numHalfSteps = (int) round( log(frequency/A4) / log(FREQ_CONST) );
-
-    int octavesFromA4 = numHalfSteps / NUM_HALF_STEPS_IN_OCTAVE;
-    int halfStepsFromA = numHalfSteps % NUM_HALF_STEPS_IN_OCTAVE;
-
-    if (halfStepsFromA + A_INDEX < 0) {
-      halfStepsFromA += 12;
-      octavesFromA4 -= 1;
-    }
-    if (halfStepsFromA + A_INDEX >= 12) {
-      halfStepsFromA -= 12;
-      octavesFromA4 += 1;
-    }
-
-    return new NoteValue(NoteName.values()[A_INDEX + halfStepsFromA], 4 + octavesFromA4);
-  }
-  */
-
-  /*
-  public NoteValue(NoteName name, int octave) {
-    this.name = name;
-    this.octave = octave;
-  }
-  */
-
   public NoteValue(float frequency) {
     this.frequency = frequency;
   }
@@ -87,24 +57,13 @@ public class NoteValue {
     return NoteName.values()[A_INDEX + halfStepsFromA];
   }
 
-  /*
-  public float getFrequency() {
-    int ind = NoteName.indexOf(name);
-    int halfStepsFromA = ind - A_INDEX;
-    int octavesFromA4 = octave - 4;
-    int halfStepsFromA4 = NUM_HALF_STEPS_IN_OCTAVE * octavesFromA4 + halfStepsFromA;
-    return (float) (A4 * pow(FREQ_CONST, halfStepsFromA4));
-  }
-  */
-
   /**
    * The line is the position on the staff where the note should lie. The top
    * space is 0, and the line below it is 1, etc. To go above the line, use
    * negative values.
    */
   public int getStaffPosition() {
-    // A4 = 4, A0 = 52, C0 = 61
-    return 61 - 12*getOctave() - NoteName.indexOf(getName());
+    return 61 - 12*getOctave() - NoteName.indexOf(getName()); // A4 = 4, A0 = 52, C0 = 61
   }
 
   @Override
--- Player/src/sheet/Staff.java
+++ Player/src/sheet/Staff.java
@@ -1,6 +1,7 @@
 package sheet;
 
 import image.Clef;
+import image.TimeSignature;
 import javafx.scene.shape.Rectangle;
 import music.NoteLength;
 import note.NoteImage;
@@ -19,9 +20,11 @@ public class Staff {
   private final List<NoteImage> notes = new ArrayList<NoteImage>();
   private final NoteImageFactory factory;
   private final Clef clef;
+  private final TimeSignature timeSignature;
 
-  public Staff(Clef clef, int x, int y) {
+  public Staff(Clef clef, TimeSignature timeSignature, int x, int y) {
     this.clef = clef;
+    this.timeSignature = timeSignature;
     factory = new NoteImageFactory(y);
     this.x = x;
     this.y = y;
@@ -29,9 +32,11 @@ public class Staff {
   }
 
   public void draw(Brush brush) {
+    clef.draw(brush, x, y-20);
+    timeSignature.draw(brush, x, y);
+
     for (NoteImage n : notes) { n.draw(brush); }
     for (int i = 0; i < 5; i++) { brush.paint(new Rectangle(x, y + LINE_GAP * i, 300, 1)); }
-    clef.draw(brush, x, y-20);
   }
 
   /**
