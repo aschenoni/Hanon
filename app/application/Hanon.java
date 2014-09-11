fd1bb0ff0b5a1bee082c64befca6793d717f9e0a
Austin Schenoni <aschenoni@gmail.com>
2014-09-11 19:55:29 -0400

    Second step UI, added menu bar at top.

--- app/application/Hanon.java
+++ app/application/Hanon.java
@@ -5,7 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
@@ -19,7 +24,7 @@ public class Hanon extends Application{
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("Hanon");
 		Button btn = new Button();
-		btn.setText("Welcome to Hanon!");
+		btn.setText("Click!");
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 			
 			@Override
@@ -28,9 +33,27 @@ public class Hanon extends Application{
 			}
 		});
 		
-		StackPane root = new StackPane();
+		VBox root = new VBox();
+		
+		MenuBar menuBar = new MenuBar();
+		Menu menuFile = new Menu("File");
+		Menu menuEdit = new Menu("Edit");
+		Menu menuView = new Menu("View");
+		Menu menuHelp = new Menu("Help");
+		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+		
+		Label label = new Label("Welcome to Hanon!");
+		
+		root.getChildren().addAll(menuBar);
+		root.getChildren().add(label);
 		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 300, 250) );
+		primaryStage.setScene(new Scene(root, 800, 600) );
+		
+		
+
+		
+		
+
 		primaryStage.show();
 	}
 }
