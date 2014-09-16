9cc02bdac6de7b26662c25e0571d0600839d64e5
Austin Schenoni <aschenoni@gmail.com>
2014-09-16 18:51:20 -0400

    changed main application driver to read in fxml layout files to better organize in the MVC pattern.

--- app/hanon/app/MainDriver.java
+++ app/hanon/app/MainDriver.java
@@ -1,57 +1,49 @@
 package hanon.app;
 
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
 
 
 public class MainDriver extends Application{
 
-	public static void main(String[] args) {
-		launch(args);
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage)
+	{
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Hanon");
+		
+		initRootLayout();
 	}
 
-	@Override
-	public void start(Stage primaryStage) {
-		primaryStage.setTitle("Hanon");
-		Button btn = new Button();
-		btn.setText("Click!");
-		btn.setOnAction(new EventHandler<ActionEvent>() {
+	private void initRootLayout() {
+		try{
+			// Load the root layout from the fxml file
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainDriver.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
 			
-			@Override
-			public void handle(ActionEvent event) {
-				System.out.println("Hello!");
-			}
-		});
-		
-		VBox root = new VBox();
-		
-		MenuBar menuBar = new MenuBar();
-		Menu menuFile = new Menu("File");
-		Menu menuEdit = new Menu("Edit");
-		Menu menuView = new Menu("View");
-		Menu menuHelp = new Menu("Help");
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
-		
-		Label label = new Label("Welcome to Hanon!");
-		
-		root.getChildren().addAll(menuBar);
-		root.getChildren().add(label);
-		root.getChildren().add(btn);
-		primaryStage.setScene(new Scene(root, 800, 600) );
-		
-		
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 
-		
-		
-
-		primaryStage.show();
+	}
+	
+	public static void main(String[] args)
+	{
+		launch(args);
 	}
 }
