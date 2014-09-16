9503d94e7b4c6a672f0b35376f91404ace8aeef5
Emily Brooks <brooks.emilymeredith@gmail.com>
2014-09-16 00:31:01 -0400

    Retrieves pitch in Hertz from audio input to microphone.

--- app/hanon/app/controller/recorder/DataRecording.java
+++ app/hanon/app/controller/recorder/DataRecording.java
@@ -1,11 +1,16 @@
 package hanon.app.controller.recorder;
 
+import be.tarsos.dsp.pitch.FFTPitch;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.Yin;
+
 import javax.sound.sampled.TargetDataLine;
 
 public class DataRecording implements Recording {
 
   private TargetDataLine targetDataLine;
-  private byte[] arr;
+  private byte[] byteArr;
+
 
   public static void main(String args[]){
     DataRecording recording = new DataRecording();
@@ -17,10 +22,17 @@ public class DataRecording implements Recording {
       e.printStackTrace();
     }
     mic.stopRecording();
-    byte[] res = recording.generateByteArray();
-    for(byte b: res){
-      System.out.print(b + " ");
+    float[] res = recording.getFloatArray();
+    for(float f: res){
+      System.out.print(f + " ");
     }
+    System.out.println();
+
+    Yin pitch = new Yin(8000, 1024);
+    PitchDetectionResult pdr = pitch.getPitch(res);
+    System.out.println(pdr.getPitch());
+    System.out.println(pdr.getProbability());
+
     mic.closeRecording();
   }
 
@@ -33,13 +45,25 @@ public class DataRecording implements Recording {
   private class CaptureThread extends Thread{
 
     public void run(){
-      arr = new byte[100000];
-      targetDataLine.read(arr, 0, 100000);
+      byteArr = new byte[2048];
+      targetDataLine.read(byteArr, 0, 2048);
     }
 
   }
 
-  public byte[] generateByteArray() {
-    return arr;
+  public byte[] getByteArray() {
+    return byteArr;
+  }
+
+  public float[] getFloatArray(){
+    float[] floatArr = new float[byteArr.length/2];
+    for (int i=0; i<byteArr.length; i+=2){
+      floatArr[i/2] = bytesToFloat(byteArr[i], byteArr[i+1]);
+    }
+    return floatArr;
+  }
+
+  public float bytesToFloat(byte b0, byte b1){
+    return (float) ((double)((b1 << 8)|(b0 & 0xFF))/32767.0);
   }
 }
