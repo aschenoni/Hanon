a171b3e63a4bc70748a781ccfac9bfbaf717f2d5
David <heath.davidanthony@gmail.com>
2014-09-17 01:38:11 -0400

    Refactored Staff to take many of the responsibilities of StaffSet. Created StaffInfo (with associated builder) to cut down on the parameters in StaffSet and Staff.

--- app/hanon/app/controller/player/staff/Staff.java
+++ app/hanon/app/controller/player/staff/Staff.java
@@ -1,26 +1,66 @@
 package hanon.app.controller.player.staff;
 
+import hanon.app.controller.music.*;
+import hanon.app.controller.player.component.NoteStem;
 import hanon.app.controller.player.sheet.Brush;
 import hanon.app.controller.player.sheet.StaffPlaceable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Staff {
   public static final int LINE_GAP = 10;
 
-  private final List<StaffPlaceable> elements;
-  private final StaffLines lines;
-  private final MeasureLine startOfStaff;
+  private final List<StaffElement> elements;
+  private final StaffInfo info;
+  private NoteImageFactory noteFactory;
 
-  public Staff(List<StaffPlaceable> elements, int x, int y, int width) {
+  public Staff(StaffInfo info, List<StaffElement> elements) {
+    this.info = info;
     this.elements = elements;
-    lines = new StaffLines(x, y, width);
-    startOfStaff = new MeasureLine(x, y, 4* LINE_GAP);
+  }
+
+  public List<StaffPlaceable> getPlaceableElements() {
+    List<Integer> spacings = new StaffSpacer(info.getWidth(), elements).getAllocatedSpacings();
+    List<StaffPlaceable> placeables = new ArrayList<StaffPlaceable>();
+    for (int i = 0; i < spacings.size(); i++)
+      placeables.add(placeElement(spacings.get(i), elements.get(i)));
+    placeables.add(0, new MeasureLine(info.getX(), info.getY(), info.getMeasureLineHeight()));
+    placeables.add(0, new StaffLines(info.getX(), info.getY(), info.getWidth()));
+    return placeables;
+  }
+
+  private StaffPlaceable placeElement(int x, StaffElement element) {
+    x += info.getX();
+    noteFactory = new NoteImageFactory(info.getY(), info.getClef());
+    switch (element.getType()) {
+      case NOTE:           return noteFactory.buildImage((MusicNote) element, x);
+      case CHORD:          return new ChordImage(getNoteImages(x, ((Chord)element).getNotes()));
+      case TIME_SIGNATURE: return new TimeSignatureImage((TimeSignature)element, x, info.getY());
+      case CLEF:           return getClef(x);
+      case MEASURE_LINE:   return new MeasureLine(x, info.getY(), info.getMeasureLineHeight());
+      case STAFF_LINES:    return new StaffLines(info.getX(), info.getX(), info.getWidth());
+      default:             throw new NoSuchStaffElementException();
+    }
+  }
+
+  private StaffPlaceable getClef(int x) {
+    if (info.getClef() == Clef.TREBLE)
+      return new ClefImage(Clef.TREBLE, x, info.getY());
+    else
+      return new ClefImage(Clef.BASS, x, info.getY());
+  }
+
+  private NoteImage[] getNoteImages(int x, MusicNote[] notes) {
+    boolean up = NoteStem.shouldStemGoUp(info.getClef(), notes);
+    NoteImage[] images = new NoteImage[notes.length];
+    for (int i = 0; i < notes.length; i++)
+      if (up) images[i] = noteFactory.buildUpImage(notes[i], x);
+      else    images[i] = noteFactory.buildDownImage(notes[i], x);
+    return images;
   }
 
   public void paint(Brush brush) {
-    for (StaffPlaceable e : elements) e.paint(brush);
-    lines.paint(brush);
-    startOfStaff.paint(brush);
+    for (StaffPlaceable e : getPlaceableElements()) e.paint(brush);
   }
 }
