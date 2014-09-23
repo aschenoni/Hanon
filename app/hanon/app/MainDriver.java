e70fd6f2fad394c9833dbeb7813a3aaeafb633ea
David <heath.davidanthony@gmail.com>
2014-09-23 10:57:34 -0400

    Small Refactorings

--- app/hanon/app/MainDriver.java
+++ app/hanon/app/MainDriver.java
@@ -5,17 +5,16 @@ import hanon.app.model.music.StaffElementSet;
 import hanon.app.model.player.sheet.MusicSheet;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class MainDriver extends Application{
@@ -30,12 +29,12 @@ public class MainDriver extends Application{
 		this.primaryStage.setTitle("Hanon");
 		
 		initRootLayout();
-		
-		drawTwinkleTwinkleLittleStar();
-	}
+
+    loadSheetMusic(new File("musicLibrary/twinkletwinkle.hanon"));
+  }
 
 	private void initRootLayout() {
-		try{
+		try {
 			// Load the root layout from the fxml file
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(MainDriver.class.getResource("view/RootLayout.fxml"));
@@ -48,28 +47,12 @@ public class MainDriver extends Application{
 			primaryStage.show();
 			rootLayout.setCenter(musicView);
 		}
-		catch (IOException e)
-		{
+		catch (IOException e) {
 			e.printStackTrace();
 		}
-
 	}
-	
-	private void drawTwinkleTwinkleLittleStar() {
 
-    List<StaffElementSet> sets = new ArrayList<StaffElementSet>();
-    sets.add(StaffElementReader.loadFromFile(new File("musicLibrary/twinkletwinkle.hanon")));
-
-    //sets.add(new StaffElementSet(Clef.TREBLE, TwinkleTwinkleLittleStar.elements));
-
-		MusicSheet twinkleMusic = new MusicSheet(FXCollections.observableArrayList(sets));
-		rootLayout.setCenter(twinkleMusic);
-		twinkleMusic.draw(500, 500); //500 is a temporary arbitrary less than 800x600 size (800x600 is main window pref size)
-		
-	}
-	
-	public static void main(String[] args)
-	{
+  public static void main(String[] args) {
 		launch(args);
 	}
 
@@ -78,8 +61,9 @@ public class MainDriver extends Application{
 	}
 
 	public void loadSheetMusic(File file) {
-		StaffElementSet ste  = StaffElementReader.loadFromFile(file);
+		StaffElementSet set  = StaffElementReader.loadFromFile(file);
 		ArrayList<StaffElementSet> list = new ArrayList<StaffElementSet>();
+    list.add(set);
 		
 		MusicSheet sheet = new MusicSheet(FXCollections.observableArrayList(list));
 		rootLayout.setCenter(sheet);
