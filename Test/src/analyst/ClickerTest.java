bbad3444caee1ef925d947e5f634de60de67941b
Austin Schenoni <aschenoni@gmail.com>
2014-12-04 16:45:22 -0500

    commented out deprecated erroneous code cause the project to have errors

--- Test/src/analyst/ClickerTest.java
+++ Test/src/analyst/ClickerTest.java
@@ -1,14 +1,14 @@
 package analyst;
 
 import hanon.app.model.analyst.rhythm.RhythmMachine;
-import hanon.app.model.analyst.rhythm.RhythmObservers;
+//import hanon.app.model.analyst.rhythm.RhythmObservers;
 import hanon.app.musicpiece.TwinkleTwinkleLittleStar;
 
 class ClickerTest {
 
   public static void main(String[] args) {
     RhythmMachine machine = RhythmMachine.fromElements(TwinkleTwinkleLittleStar.elements, 140);
-    machine.register(RhythmObservers.clicker);
+    //machine.register(RhythmObservers.clicker);
     Thread th = new Thread(machine);
     th.setDaemon(true);
     th.start();
