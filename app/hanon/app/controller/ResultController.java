75a53535543af07b0561d56307a85fe3caf44f9d
Austin Schenoni <aschenoni@gmail.com>
2014-12-04 16:28:18 -0500

    Medal images, initial features of the result controller

--- app/hanon/app/controller/ResultController.java
+++ app/hanon/app/controller/ResultController.java
@@ -2,18 +2,43 @@ package hanon.app.controller;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import hanon.app.MainDriver;
 import hanon.app.model.analyst.results.SongResult;
 
 public class ResultController extends BaseController {
 
+	private MainDriver mainDriver;
+	private SongResult result;
+	
+	/*****FXML OBJECTS******/
+	@FXML Label resultTitle;
+	@FXML Label comparedTo;
+	@FXML Label goodPercent;
+	@FXML Label badPercent;
+	@FXML Label unplayedPercent;
+	@FXML PieChart pchart;
+	@FXML ImageView profile;
+	@FXML ImageView medal;
+	@FXML BarChart level;
+	
 	public ResultController() {
 		
 	}
 	
-	private MainDriver mainDriver;
+	public void setMainDriver(MainDriver mainDriver) {
+		this.mainDriver = mainDriver;
+	}
+	
+	public void setSongResult(SongResult result) {
+		this.result = result;
+	}
 
 }
